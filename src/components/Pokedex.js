6c0e562cb516fe4f287d0c746b58dae40d067067
Omar Hawas <omarhawas94@gmail.com>
2022-06-30 13:32:30 -0400

    next page and previos page api calls working. able to retrieve all data in pokedex component

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -3,14 +3,16 @@ import axios from "axios";
 
 const Pokedex = () => {
   const [pokemonList, setPokemonList] = useState([]);
+  const [nextPokemon, setNextPokemon] = useState("");
+  const [previousPokemon, setPreviousPokemon] = useState("");
 
   const getPokemon = async () => {
     const result = await axios.get(
       "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
     );
-    const nextResult = result.data.next;
-    const previousResult = result.data.previous;
     setPokemonList(result.data.results);
+    setNextPokemon(result.data.next);
+    setPreviousPokemon(result.data.previous);
     console.log(result);
   };
 
@@ -18,15 +20,19 @@ const Pokedex = () => {
     getPokemon();
   }, []);
 
-  //   const findPreviousPokemon = async () => {
-  //     const result = await axios.get(previousResult);
-  //     setPokemonList(result.data.results);
-  //   };
+  const findPreviousPokemon = async () => {
+    const result = await axios.get(previousPokemon);
+    setPokemonList(result.data.results);
+    setPreviousPokemon(result.data.previous);
+    setNextPokemon(result.data.next);
+  };
 
-  //   const findNextPokemon = async () => {
-  //     const result = await axios.get(nextResult);
-  //     setPokemonList(result.data.results);
-  //   };
+  const findNextPokemon = async () => {
+    const result = await axios.get(nextPokemon);
+    setPokemonList(result.data.results);
+    setPreviousPokemon(result.data.previous);
+    setNextPokemon(result.data.next);
+  };
 
   return (
     <div>
@@ -37,12 +43,12 @@ const Pokedex = () => {
           console.log(p);
         })}
       </ul>
-      {/* <button className="previousBtn" onClick={findPreviousPokemon}>
+      <button className="previousBtn" onClick={findPreviousPokemon}>
         Previous
       </button>
       <button className="nextBtn" onClick={findNextPokemon}>
         Next
-      </button> */}
+      </button>
     </div>
   );
 };
