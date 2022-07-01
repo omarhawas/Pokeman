e60e598fa66e23321e07694f261ab6856d7c24d8
Omar Hawas <omarhawas94@gmail.com>
2022-07-01 14:32:39 -0400

    pokemonpage working. individual pokemons showing in console not yet in UI.

--- src/pages/pokemonPage.js
+++ src/pages/pokemonPage.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-const pokemonPage = () => {
+const PokemonPage = () => {
+  const [pokemon, setPokemon] = useState("");
+  const pokemonName = useParams().name;
+
+  const getPokemon = async () => {
+    const result = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    console.log(result.data);
+    setPokemon(result.data);
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      {/* <p>{pokemon.abilities}</p> */}
     </div>
   );
 };
 
-export default pokemonPage;
+export default PokemonPage;
